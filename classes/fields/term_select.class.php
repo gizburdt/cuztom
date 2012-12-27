8dc48f09e352bc590b862a24a6870191ae974550
Gijs Jorissen <gizburdt@gmail.com>
2012-12-27 16:17:52 +0100

    Fixed bug with non-populated term_select

--- classes/fields/term_select.class.php
+++ classes/fields/term_select.class.php
@@ -2,6 +2,8 @@
 
 class Cuztom_Field_Term_Select extends Cuztom_Field
 {
+	var $dropdown;
+
 	function __construct( $field, $meta_box )
 	{
 		parent::__construct( $field, $meta_box );
@@ -22,6 +24,8 @@ class Cuztom_Field_Term_Select extends Cuztom_Field
 		$this->options['class'] .= ' cuztom-input';
 		$this->options['name'] 	= 'cuztom[' . $this->id_name . ']' . ( $this->repeatable ? '[]' : '' );
 		$this->options['echo']	= 0;
+
+		add_action( 'init', array( &$this, 'get_dropdown' ) );
 	}
 
 	function _output( $value )
@@ -29,7 +33,7 @@ class Cuztom_Field_Term_Select extends Cuztom_Field
 		$this->options['selected'] 	= ( ! empty( $value ) ? $value : $this->default_value );
 
 		$output = ( $this->repeatable ? '<li class="cuztom-field cuztom-sortable-item js-cuztom-sortable-item"><div class="cuztom-handle-sortable js-cuztom-handle-sortable"></div>' : '' );
-			$output .= wp_dropdown_categories( $this->options );
+			$output .= $this->dropdown;
 		$output .= ( $this->repeatable ? '</li>' : '' );
 
 		return $output;
@@ -44,10 +48,22 @@ class Cuztom_Field_Term_Select extends Cuztom_Field
 			$this->options['selected'] = ( ! empty( $item ) ? $item : $this->default_value );
 			
 			$output .= '<li class="cuztom-field cuztom-sortable-item js-cuztom-sortable-item"><div class="cuztom-handle-sortable js-cuztom-handle-sortable"></div>';
-				$output .= wp_dropdown_categories( $this->options );
+				$output .= $this->dropdown;
 			$output .= ( count( $value ) > 1 ? '<div class="js-cuztom-remove-sortable cuztom-remove-sortable"></div>' : '' ) . '</li>';
 		}
 
 		return $output;
-	}	
+	}
+
+	/**
+	 * Gets taxonomy terms for use in the output
+	 * 
+	 * @author 	Gijs Jorissen
+	 * @since 	2.0
+	 * 
+	 */
+	function get_dropdown()
+    {
+        $this->dropdown = wp_dropdown_categories( $this->options );
+    }
 }
