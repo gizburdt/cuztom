de8d692f6eb0b1fc1233bcf4886d1323714100bf
Gijs Jorissen <gizburdt@gmail.com>
2013-03-09 16:07:00 +0100

    Select in bundles

--- classes/fields/select.class.php
+++ classes/fields/select.class.php
@@ -4,10 +4,11 @@ class Cuztom_Field_Select extends Cuztom_Field
 {
 	var $_supports_repeatable 	= true;
 	var $_supports_ajax			= true;
+	var $_supports_bundle		= true;
 	
 	function _output( $value )
 	{
-		$output = '<select name="cuztom[' . $this->id_name . ']' . $this->after . '" id="' . $this->id_name . '" class="cuztom-input">';
+		$output = '<select name="cuztom' . $this->pre . '[' . $this->id_name . ']' . $this->after . '" id="' . $this->id_name . '" class="cuztom-input">';
 			if( isset( $this->args['option_none'] ) && $this->args['option_none'] )
 				$output .= '<option value="0" ' . ( empty( $value ) ? 'selected="selected"' : '' ) . '>' . __( 'None', 'cuztom' ) . '</option>';				
 
