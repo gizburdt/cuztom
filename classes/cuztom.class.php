d472d06fc0318ffc593d6fb8bd0b29b6ea8de50a
Gijs Jorissen <gizburdt@gmail.com>
2013-04-01 01:06:28 +0200

    Improved Cuztom::pluralize()

--- classes/cuztom.class.php
+++ classes/cuztom.class.php
@@ -226,23 +226,65 @@ class Cuztom
 	 */
 	static function pluralize( $string )
 	{
-		$last = $string[strlen( $string ) - 1];
+		$plural = array(
+            array( '/(quiz)$/i',               "$1zes"   ),
+        	array( '/^(ox)$/i',                "$1en"    ),
+        	array( '/([m|l])ouse$/i',          "$1ice"   ),
+        	array( '/(matr|vert|ind)ix|ex$/i', "$1ices"  ),
+        	array( '/(x|ch|ss|sh)$/i',         "$1es"    ),
+        	array( '/([^aeiouy]|qu)y$/i',      "$1ies"   ),
+        	array( '/([^aeiouy]|qu)ies$/i',    "$1y"     ),
+            array( '/(hive)$/i',               "$1s"     ),
+            array( '/(?:([^f])fe|([lr])f)$/i', "$1$2ves" ),
+            array( '/sis$/i',                  "ses"     ),
+            array( '/([ti])um$/i',             "$1a"     ),
+            array( '/(buffal|tomat)o$/i',      "$1oes"   ),
+            array( '/(bu)s$/i',                "$1ses"   ),
+            array( '/(alias|status)$/i',       "$1es"    ),
+            array( '/(octop|vir)us$/i',        "$1i"     ),
+            array( '/(ax|test)is$/i',          "$1es"    ),
+            array( '/s$/i',                    "s"       ),
+            array( '/$/',                      "s"       )
+        );
+
+        $irregular = array(
+	        array( 'move',   'moves'    ),
+	        array( 'sex',    'sexes'    ),
+	        array( 'child',  'children' ),
+	        array( 'man',    'men'      ),
+	        array( 'person', 'people'   )
+        );
+
+        $uncountable = array( 
+	        'sheep', 
+	        'fish',
+	        'series',
+	        'species',
+	        'money',
+	        'rice',
+	        'information',
+	        'equipment'
+        );
+
+        // Save time if string in uncountable
+        if ( in_array( strtolower( $string ), $uncountable ) )
+        	return apply_filters( 'cuztom_pluralize', $string );
+
+        // Check for irregular words
+        foreach ( $irregular as $noun )
+        {
+        	if ( strtolower( $string ) == $noun[0] )
+            	return apply_filters( 'cuztom_pluralize', $noun[1] );
+        }
+
+        // Check for plural forms
+        foreach ( $plural as $pattern )
+        {
+        	if ( preg_match( $pattern[0], $string ) )
+        		return apply_filters( 'cuztom_pluralize', preg_replace( $pattern[0], $pattern[1], $string ) );
+        }
 		
-		if( $last != 's' )
-		{
-			if( $last == 'y' )
-			{
-				$cut = substr( $string, 0, -1 );
-				//convert y to ies
-				$string = $cut . 'ies';
-			}
-			else
-			{
-				// just attach a s
-				$string = $string . 's';
-			}
-		}
-		
+		// Return if noting found
 		return apply_filters( 'cuztom_pluralize', $string );
 	}
 
