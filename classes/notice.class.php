d3763b58b48f3731cb92283baac02ed3ee71396f
Gijs Jorissen <gizburdt@gmail.com>
2013-04-01 02:15:06 +0200

    Docs for Cuztom_Notice

--- classes/notice.class.php
+++ classes/notice.class.php
@@ -1,10 +1,28 @@
 <?php
 
+if( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Cuztom notice class, to easily handle admin notices
+ *
+ * @author  Gijs Jorissen
+ * @since  	2.3
+ */
 class Cuztom_Notice
 {
 	var $notice;
 	var $type;
 
+	/**
+	 * Constructor
+	 * 
+	 * @param  	string 	$notice 
+	 * @param 	string 	$type
+	 *
+	 * @author  Gijs Jorissen
+	 * @since   2.3
+	 * 
+	 */
 	function __construct( $notice, $type = 'updated' )
 	{
 		$this->notice 	= $notice;
@@ -13,6 +31,13 @@ class Cuztom_Notice
 		add_action( 'admin_notices', array( &$this, 'add_admin_notice' ) );
 	}
 
+	/**
+	 * Adds the admin notice
+	 *
+	 * @author 	Gijs Jorissen
+	 * @since   2.3
+	 * 
+	 */
 	function add_admin_notice()
 	{
 		echo '<div class="' . $this->type . '">';
