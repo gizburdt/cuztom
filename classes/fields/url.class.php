1c3216bf73ecea380dc22f3f18cb848a01539dc1
Gijs Jorissen <gizburdt@gmail.com>
2013-05-13 23:24:20 +0200

    Small updates for url field

--- classes/fields/url.class.php
+++ classes/fields/url.class.php
@@ -4,6 +4,10 @@ if( ! defined( 'ABSPATH' ) ) exit;
 
 class Cuztom_Field_Url extends Cuztom_Field
 {
+	var $_supports_ajax			= true;
+
+	var $css_classes			= array( 'js-cuztom-url', 'cuztom-url', 'cuztom-input' );
+
 	function _output( $value, $object )
 	{
 		
