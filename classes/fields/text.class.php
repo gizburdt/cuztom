4359235aa94fdaf243dc3fa7101841fbecb817f6
Gijs Jorissen <gizburdt@gmail.com>
2013-05-19 23:27:25 +0200

    htmlspecialchars for text field

--- classes/fields/text.class.php
+++ classes/fields/text.class.php
@@ -9,4 +9,11 @@ class Cuztom_Field_Text extends Cuztom_Field
 	var $_supports_ajax			= true;
 
 	var $css_classes			= array( 'cuztom-input' );
+
+	function save( $post_id, $value, $meta_type )
+	{
+		$value = htmlspecialchars( $value );
+
+		parent::save( $post_id, $value, $meta_type );
+	}
 }
