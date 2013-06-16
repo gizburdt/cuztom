6dc0b2cbfc4a94c30800e684bc1d6eaf6f47dc9a
Gijs Jorissen <gizburdt@gmail.com>
2013-06-16 22:05:50 +0200

    Strtotime for date and datetime

--- classes/fields/date.class.php
+++ classes/fields/date.class.php
@@ -8,4 +8,11 @@ class Cuztom_Field_Date extends Cuztom_Field
 
 	var $css_classes			= array( 'js-cuztom-datepicker', 'cuztom-datepicker', 'datepicker', 'cuztom-input' );
 	var $data_attributes 		= array( 'date-format' => null );
+
+	function save( $post_id, $value, $meta_type )
+	{
+		$value = strtotime( $value );
+
+		return parent::save( $post_id, $value, $meta_type );
+	}
 }
--- classes/fields/datetime.class.php
+++ classes/fields/datetime.class.php
@@ -8,4 +8,11 @@ class Cuztom_Field_Datetime extends Cuztom_Field
 
 	var $css_classes			= array( 'js-cuztom-datetimepicker', 'cuztom-datetimepicker', 'datetimepicker', 'cuztom-input' );
 	var $data_attributes		= array( 'time-format' => null, 'date-format' => null );
+
+	function save( $post_id, $value, $meta_type )
+	{
+		$value = strtotime( $value );
+
+		return parent::save( $post_id, $value, $meta_type );
+	}
 }
