7e98d498e9fb5b741eebf224aef385364a8610cf
Gijs Jorissen <gizburdt@gmail.com>
2013-06-16 22:17:24 +0200

    Right date output for date field

--- classes/fields/date.class.php
+++ classes/fields/date.class.php
@@ -9,6 +9,11 @@ class Cuztom_Field_Date extends Cuztom_Field
 	var $css_classes			= array( 'js-cuztom-datepicker', 'cuztom-datepicker', 'datepicker', 'cuztom-input' );
 	var $data_attributes 		= array( 'date-format' => null );
 
+	function _output( $value, $object )
+	{
+		return '<input type="text" ' . $this->output_name() . ' ' . $this->output_id() . ' ' . $this->output_css_class() . ' value="' . ( ! empty( $value ) ? ( isset( $this->args['date_format'] ) ? date( $this->args['date_format'], $value ) : date( 'm/d/Y', $value ) ) : $this->default_value ) . '" ' . $this->output_data_attributes() . ' />' . $this->output_explanation();
+	}
+
 	function save( $post_id, $value, $meta_type )
 	{
 		$value = strtotime( $value );
