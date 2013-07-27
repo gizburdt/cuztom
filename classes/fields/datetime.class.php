1f7eb8ec6895017308cc17da029dd11832ceb1a1
Gijs Jorissen <gizburdt@gmail.com>
2013-07-27 14:59:37 +0200

    Start of date/time format parsing

--- classes/fields/datetime.class.php
+++ classes/fields/datetime.class.php
@@ -11,12 +11,22 @@ class Cuztom_Field_Datetime extends Cuztom_Field
 
 	function _output( $value )
 	{
-		return '<input type="text" ' . $this->output_name() . ' ' . $this->output_id() . ' ' . $this->output_css_class() . ' value="' . ( ! empty( $value ) ? ( isset( $this->args['date_format'] ) && isset( $this->args['time_format'] ) ? date( $this->args['date_format'] . ' ' . $this->args['time_format'], $value ) : date( 'm/d/Y H:i', $value ) ) : $this->default_value ) . '" ' . $this->output_data_attributes() . ' />' . $this->output_explanation(); }
+		return '<input type="text" ' . $this->output_name() . ' ' . $this->output_id() . ' ' . $this->output_css_class() . ' value="' . ( ! empty( $value ) ? ( isset( $this->args['date_format'] ) && isset( $this->args['time_format'] ) ? date( $this->parse_date_format( $this->args['date_format'] ) . ' ' . $this->parse_time_format( $this->args['time_format'] ), $value ) : date( 'm/d/Y H:i', $value ) ) : $this->default_value ) . '" ' . $this->output_data_attributes() . ' />' . $this->output_explanation(); }
 
 	function save( $post_id, $value )
 	{
 		$value = strtotime( $value );
 
 		return parent::save( $post_id, $value );
+	}
+
+	function parse_date_format( $date )
+	{
+		return $date;
+	}
+
+	function parse_time_format( $time )
+	{
+		return $time;
 	}
 }
