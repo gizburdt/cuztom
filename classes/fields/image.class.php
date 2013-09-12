fc321fc211a438eec0f368ff5e72db4381280983
Ante Primorac <anteprimorachr@gmail.com>
2013-09-12 11:55:34 +0200

    Image preview size
    Usage:
    
    'image' => array(
    'name' => 'image',
    'label' => 'Image',
    'type' => 'image',
    'args' => array(
    'size' => 'large'
    )
    )
    or array :
    
    'image' => array(
    'name' => 'image',
    'label' => 'Image',
    'type' => 'image',
    'args' => array(
    'size' => array(100, 100)
    )
    )
    
    if you want use custom image sizes, add this filter:
    
    add_filter( 'image_size_names_choose', function( $sizes ) {
    $possible_sizes = get_intermediate_image_sizes();
    unset( $possible_sizes['full'] );
    if( !empty( $possible_sizes ) )
    return $possible_sizes;
    else
    return $sizes;
    } );

--- classes/fields/image.class.php
+++ classes/fields/image.class.php
@@ -15,7 +15,7 @@ class Cuztom_Field_Image extends Cuztom_Field
 
 		if( ! empty( $value ) )
 		{
-			$url = wp_get_attachment_image_src( $value, apply_filters( 'cuztom_preview_size', 'medium' ) );
+			$url = wp_get_attachment_image_src( $value, !empty($this->args["preview_size"]) ? $this->args["preview_size"] : apply_filters( 'cuztom_preview_size', 'medium' ) );
 			$url = $url[0];
 			$image  = '<img src="' . $url . '" />';
 		}
@@ -25,7 +25,7 @@ class Cuztom_Field_Image extends Cuztom_Field
 		}
 	
 		$output .= '<input type="hidden" ' . $this->output_name() . ' ' . $this->output_id() . ' ' . $this->output_css_class() . ' value="' . ( ! empty( $value ) ? $value : '' ) . '" />';
-		$output .= sprintf( '<input id="upload-image-button" type="button" class="button js-cuztom-upload" data-cuztom-media-type="image" value="%s" />', __( 'Select image', 'cuztom' ) );
+		$output .= sprintf( '<input id="upload-image-button" type="button" class="button js-cuztom-upload" data-cuztom-media-type="image"' . (!empty($this->args["preview_size"]) ? ' data-cuztom-media-preview-size="' . ( is_array($this->args["preview_size"]) ? esc_attr( json_encode($this->args["preview_size"]) ) : $this->args["preview_size"] ) . '"' : '') . ' value="%s" />', __( 'Select image', 'cuztom' ) );
 		$output .= ( ! empty( $value ) ? sprintf( '<a href="#" class="js-cuztom-remove-media cuztom-remove-media">%s</a>', __( 'Remove current image', 'cuztom' ) ) : '' );
 
 		$output .= '<span class="cuztom-preview">' . $image . '</span>';
