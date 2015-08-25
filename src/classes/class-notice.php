b6aed27dbde9f3b1d2ecca5530597b4f67da0927
Gijs Jorissen <gizburdt@gmail.com>
2015-08-25 23:39:41 +0200

    Dismissible notice

--- src/classes/class-notice.php
+++ src/classes/class-notice.php
@@ -12,21 +12,23 @@ class Cuztom_Notice
 {
 	var $notice;
 	var $type;
+	var $dismissible;
 
 	/**
 	 * Constructor
-	 * 
-	 * @param  	string 	$notice 
+	 *
+	 * @param  	string 	$notice
 	 * @param 	string 	$type
 	 *
 	 * @author  Gijs Jorissen
 	 * @since   2.3
-	 * 
+	 *
 	 */
-	function __construct( $notice, $type = 'updated' )
+	function __construct( $notice, $type = 'updated', $dismissible )
 	{
-		$this->notice 	= $notice;
-		$this->type 	= $type;
+		$this->notice 		= $notice;
+		$this->type 		= $type;
+		$this->dismissible	= $dismissible;
 
 		add_action( 'admin_notices', array( &$this, 'add_admin_notice' ) );
 	}
@@ -36,12 +38,30 @@ class Cuztom_Notice
 	 *
 	 * @author 	Gijs Jorissen
 	 * @since   2.3
-	 * 
+	 *
 	 */
 	function add_admin_notice()
 	{
-		echo '<div class="' . $this->type . '">';
+		echo '<div class="' . $this->get_css_class() . '">';
 			echo '<p>' . $this->notice . '</p>';
     	echo '</div>';
+	}
+
+	/**
+	 * Returns the complete css class for the notice
+	 *
+	 * @author 	Gijs Jorissen
+	 * @since   2.3
+	 *
+	 */
+	function get_css_class()
+	{
+		$class = $this->type;
+
+		if($this->dismissible) {
+			$class .= ' is-dismissible';
+		}
+
+		return $class;
 	}
 }
