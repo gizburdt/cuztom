60c55fba2732b612a8424a143fdd64a62db1b407
Gijs Jorissen <gizburdt@gmail.com>
2015-08-26 00:01:55 +0200

    Fix array to string conversion

--- src/classes/class-taxonomy.php
+++ src/classes/class-taxonomy.php
@@ -17,7 +17,7 @@ class Cuztom_Taxonomy
 	var $labels;
 	var $args;
 	var $post_type;
-	
+
 	/**
 	 * Constructs the class with important vars and method calls
 	 * If the taxonomy exists, it will be attached to the post type
@@ -33,24 +33,21 @@ class Cuztom_Taxonomy
 	 */
 	function __construct( $name, $post_type = null, $args = array(), $labels = array() )
 	{
-		// Build name
-		if( ! empty( $name ) ) {
-			if( is_array( $name ) ) {
-				$this->name		= Cuztom::uglify( $name[0] );
-				$this->title	= Cuztom::beautify( $name[0] );
-				$this->plural 	= Cuztom::beautify( $name[1] );
-			} else {
-				$this->name		= Cuztom::uglify( $name );
-				$this->title	= Cuztom::beautify( $name );
-				$this->plural 	= Cuztom::pluralize( Cuztom::beautify( $name ) );
-			}
+		if( is_array( $name ) ) {
+			$this->name		= Cuztom::uglify( $name[0] );
+			$this->title	= Cuztom::beautify( $name[0] );
+			$this->plural 	= Cuztom::beautify( $name[1] );
+		} else {
+			$this->name		= Cuztom::uglify( $name );
+			$this->title	= Cuztom::beautify( $name );
+			$this->plural 	= Cuztom::pluralize( Cuztom::beautify( $name ) );
 		}
 
 		// Set properties
-		$this->post_type 	= $post_type;
+		$this->post_type 	= (array) $post_type;
 		$this->labels		= $labels;
 		$this->args			= $args;
-        
+
         // Register taxonomy
         if( ! taxonomy_exists( $this->name ) ) {
         	add_action( 'init', array( &$this, 'register_taxonomy' ) );
@@ -60,16 +57,18 @@ class Cuztom_Taxonomy
 
 		// Sortable columns
 		if( @$args['admin_column_sortable'] ) {
-			add_action( "manage_edit-{$post_type}_sortable_columns", array( &$this, 'add_sortable_column' ) );
+			foreach($this->post_type as $post_type) :
+				add_action( "manage_edit-{$post_type}_sortable_columns", array( &$this, 'add_sortable_column' ) );
+			endforeach;
 		}
 
 		// Column filter
 		if( @$args['admin_column_filter'] ) {
-			add_action( 'restrict_manage_posts', array( &$this, 'admin_column_filter' ) ); 
+			add_action( 'restrict_manage_posts', array( &$this, 'admin_column_filter' ) );
 			add_filter( 'parse_query', array( &$this, '_post_filter_query') );
 		}
 	}
-	
+
 	/**
 	 * Registers the custom taxonomy with the given arguments
 	 *
@@ -90,7 +89,7 @@ class Cuztom_Taxonomy
 				    'all_items' 			=> sprintf( __( 'All %s', 'cuztom' ), $this->plural ),
 				    'parent_item' 			=> sprintf( __( 'Parent %s', 'cuztom' ), $this->title ),
 				    'parent_item_colon' 	=> sprintf( __( 'Parent %s:', 'cuztom' ), $this->title ),
-				    'edit_item' 			=> sprintf( __( 'Edit %s', 'cuztom' ), $this->title ), 
+				    'edit_item' 			=> sprintf( __( 'Edit %s', 'cuztom' ), $this->title ),
 				    'update_item' 			=> sprintf( __( 'Update %s', 'cuztom' ), $this->title ),
 				    'add_new_item' 			=> sprintf( __( 'Add New %s', 'cuztom' ), $this->title ),
 				    'new_item_name' 		=> sprintf( __( 'New %s Name', 'cuztom' ), $this->title ),
@@ -112,11 +111,11 @@ class Cuztom_Taxonomy
 				),
 				$this->args
 			);
-			
+
 			register_taxonomy( $this->name, $this->post_type, $args );
 		}
 	}
-	
+
 	/**
 	 * Used to attach the existing taxonomy to the post type
 	 *
@@ -161,10 +160,10 @@ class Cuztom_Taxonomy
 
 		$columns[$this->name] = $this->title;
 		$columns['date'] = __( 'Date', 'cuztom' );
-		
+
 		return $columns;
 	}
-	
+
 	/**
 	 * Used to add the column content to the column head
 	 *
@@ -186,13 +185,13 @@ class Cuztom_Taxonomy
 
 	/**
 	 * Used to make all columns sortable
-	 * 
+	 *
 	 * @param 	array 			$columns
 	 * @return  array
 	 *
 	 * @author  Gijs Jorissen
 	 * @since   1.6
-	 * 
+	 *
 	 */
 	function add_sortable_column( $columns )
 	{
@@ -203,12 +202,12 @@ class Cuztom_Taxonomy
 
 	/**
 	 * Adds a filter to the post table filters
-	 * 
+	 *
 	 * @author 	Gijs Jorissen
 	 * @since 	1.6
-	 * 
+	 *
 	 */
-	function admin_column_filter() 
+	function admin_column_filter()
 	{
 		global $typenow, $wp_query;
 
@@ -228,14 +227,14 @@ class Cuztom_Taxonomy
 
 	/**
 	 * Applies the selected filter to the query
-	 * 
+	 *
 	 * @param 	object 			$query
 	 *
 	 * @author  Gijs Jorissen
 	 * @since  	1.6
-	 * 
+	 *
 	 */
-	function _post_filter_query( $query ) 
+	function _post_filter_query( $query )
 	{
     	global $pagenow;
     	$vars = &$query->query_vars;
