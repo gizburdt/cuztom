26a252ea826fca209a892c925589b98f9479de3c
Gijs Jorissen <gizburdt@gmail.com>
2015-11-03 20:21:00 +0100

    Spaces

--- src/functions/post-type.php
+++ src/functions/post-type.php
@@ -3,34 +3,34 @@
 if( ! defined( 'ABSPATH' ) ) exit;
 
 if( ! function_exists( 'register_cuztom_post_type' ) ) {
-	/**
-	 * Registers a Post Type
-	 *
-	 * @param 	string|array 	$name
-	 * @param 	array 			$args
-	 * @param 	array 			$labels
-	 * @return 	object 			Cuztom_Post_Type
-	 *
-	 * @author 	Gijs Jorissen
-	 * @since 	0.8
-	 *
-	 */
-	function register_cuztom_post_type( $name, $args = array(), $labels = array() )
-	{
-		return new Cuztom_Post_Type( $name, $args, $labels );
-	}
+    /**
+     * Registers a Post Type
+     *
+     * @param   string|array    $name
+     * @param   array           $args
+     * @param   array           $labels
+     * @return  object          Cuztom_Post_Type
+     *
+     * @author  Gijs Jorissen
+     * @since   0.8
+     *
+     */
+    function register_cuztom_post_type( $name, $args = array(), $labels = array() )
+    {
+        return new Cuztom_Post_Type( $name, $args, $labels );
+    }
 }
 
 if( ! function_exists( 'register_cuztom_meta_box' ) ) {
-	/**
-	 * Register met box
-	 * @param  string 		$id
-	 * @param  array  		$data
-	 * @param  string|array $post_type
-	 * @return object
-	 */
-	function register_cuztom_meta_box( $id, $data = array(), $post_type )
-	{
-		return new Cuztom_Meta_Box( $id, $data, $post_type );
-	}
+    /**
+     * Register met box
+     * @param  string       $id
+     * @param  array        $data
+     * @param  string|array $post_type
+     * @return object
+     */
+    function register_cuztom_meta_box( $id, $data = array(), $post_type )
+    {
+        return new Cuztom_Meta_Box( $id, $data, $post_type );
+    }
 }
