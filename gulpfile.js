d2815d20cef44974ec84b37f22bbc149058596aa
Gijs Jorissen <gizburdt@gmail.com>
2016-11-10 17:13:40 +0100

    Prefix

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,12 @@ var elixir = require('laravel-elixir');
 elixir.config.sourcemaps = false;
 
 elixir(function(mix) {
-    mix
-
-    .less([
+    mix.less([
         'vendor/datetimepicker/jquery.datetimepicker.css',
         'less/cuztom.less',
-    ], 'assets/dist/css/cuztom.min.css', 'assets/')
+    ], 'assets/dist/css/cuztom.min.css', 'assets/');
 
-    .scripts([
+    mix.scripts([
         'vendor/vue/dist/vue.js',
         'vendor/datetimepicker/build/jquery.datetimepicker.full.js',
         'js/cuztom-open.js',
@@ -18,5 +16,10 @@ elixir(function(mix) {
         'js/cuztom-sortable.js',
         'js/cuztom-media.js',
         'js/cuztom-close.js',
-    ], 'assets/dist/js/cuztom.min.js', 'assets/')
+    ], 'assets/dist/js/cuztom.min.js', 'assets/');
+});
+
+gulp.task('prefix', function(){
+    gulp.src('**')
+        .pipe(replace('Gizburdt', 'Gizzy');
 });
